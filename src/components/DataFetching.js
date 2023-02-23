fc182bf7846fd222991c2571e5cc13a25d8cacc1
ShihanVidulanka <81677102+ShihanVidulanka@users.noreply.github.com>
2023-02-23 12:25:56 +0530

    Added transpose of the table

--- src/components/DataFetching.js
+++ src/components/DataFetching.js
@@ -2,33 +2,48 @@ import React, { useState } from "react";
 import axios from "axios";
 function DataFetching() {
     const [dataItems, setDataItems] = useState([]);
-    const [url, setUrl] = useState("https://jsonplaceholder.typicode.com/posts/1/users");
+    const [url, setUrl] = useState(
+        "https://jsonplaceholder.typicode.com/users"
+    );
+    const [row, setRow] = useState([])
     const [column, setColumn] = useState([]);
-    const [error, setError] = useState('');
+    const [error, setError] = useState("");
+    const [transpose, setTranspose] = useState(false);
     const handleSubmit = (event) => {
         event.preventDefault();
         axios
             .get(url)
             .then((res) => {
                 console.log(res);
-                if (Array.isArray(res.data)){   //if the response is a array of json objects.
-                    isJsonObject(res.data[0]);
-                    setDataItems(res.data);
-                    setColumn(Object.keys(res.data[0]));
-                }else{  //if the response is only a json object
-                    setDataItems(new Array(res.data));
-                    setColumn(Object.keys(res.data));
-                }
-                setError('');
+                handelArray(res.data)
+                setError("");
             })
             .catch((err) => {
                 console.log(err);
                 setError(err);
                 setDataItems([]);
+                setRow([]);
                 setColumn([]);
+                setTranspose(false);
             });
     };
-
+    const handelArray = (data) => {
+        if (Array.isArray(data)) {
+            //if the response is a array of json objects.
+            isJsonObject(data[0]);
+            setDataItems(data);
+            setRow(data)
+            setColumn(Object.keys(data[0]));
+            setTranspose(false);
+            console.log(row)
+        } else {
+            //if the response is only a json object
+            const info = new Array(data);
+            setDataItems(info);
+            setRow(info);
+            setColumn(Object.keys(data));
+        }
+    }
     // get table heading data
     const ThData = () => {
         return column.map((data) => {
@@ -37,15 +52,44 @@ function DataFetching() {
     };
     // get table row data
     const tdData = () => {
-        return dataItems.map((data) => {
+        return row.map((data) => {
             return (
-                <tr key = {data.id+1000} >
+                <tr key={data.id + 1000}>
                     {column.map((v) => {
-                        return <td key = {data.id + v}>{JSON.stringify(data[v]).replace(/"/g, " ")}</td>;
+                        return (
+                            <td key={data.id + v}>
+                                {JSON.stringify(data[v]).replace(/"/g, " ")}
+                            </td>
+                        );
                     })}
                 </tr>
             );
         });
+    };
+
+    const handleTranspose = () => {
+        if (!transpose) {
+            var rows = new Array();
+            dataItems.forEach(function (_value, i) {
+                for (const [key, value]  of Object.entries(_value)) {
+                    rows.push({
+                        "id" : key + i,
+                        "key": key,
+                        "value": value
+                    });
+                }
+                rows.push({
+                    "id" : i,
+                    "key": "",
+                    "value": ""
+                })
+            });
+            setColumn(["key", "value"]);
+            setRow(rows);
+            setTranspose(!transpose);
+        } else {
+            handelArray(dataItems)
+        }
     };
 
     //  check is the object is json or not.
@@ -56,7 +100,7 @@ function DataFetching() {
             return false;
         }
         return true;
-    };
+    }
 
     return (
         <div>
@@ -70,19 +114,21 @@ function DataFetching() {
                         />
                     </label>
                     <input type="submit" value="Submit" />
-                    <div>
-                        {error !== '' && <p>{error.message}</p>}
-                    </div>
+                    <input type="button" value="Transpose" onClick={handleTranspose} />
+
+                    <div>{error !== "" && <p>{error.message}</p>}</div>
                 </form>
             </div>
             <div>
-                <table className="table table-bordered table-striped table-dark" variant="dark" size="sm">
+                <table
+                    className="table table-bordered table-striped table-dark"
+                    variant="dark"
+                    size="sm"
+                >
                     <thead>
-                        <tr >{ThData()}</tr>
+                        <tr>{ThData()}</tr>
                     </thead>
-                    <tbody>
-                        {tdData()}
-                    </tbody>
+                    <tbody>{tdData()}</tbody>
                 </table>
             </div>
         </div>
@@ -98,5 +144,5 @@ export default DataFetching;
 // https://jsonplaceholder.typicode.com/todos
 // https://jsonplaceholder.typicode.com/comments
 
-
 // https://jsonplaceholder.typicode.com/
+// http://validate.jsontest.com/?json=%7B%22key%22:%22value%22%7D
